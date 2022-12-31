118c2dd4dbd0839226c58f990c1fd0c0bc7d582a
Mason Wheeler <mwheel@uw.edu>
2022-12-30 22:19:46 -0800

    iterating

--- src/routes/login/+page.ts
+++ src/routes/login/+page.ts
@@ -1,13 +1,5 @@
 // imports
-import {goto} from "$app/navigation";
-import { error } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
 
 // check if their is a current user, if so redirect back to "/"
 // if not, render the login page
-export const load: PageLoad = ({session}) => {
-    if (session) {
-        goto('/');
-    }
-    return {};
-}
+
